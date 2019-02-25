e063eda066738e436116aab3c38ca220ec88f1f7
Rodrigo Mariano <rodmariano13@gmail.com>
2019-02-25 16:58:31 -0300

    Trying to add layer

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="explore_main">
+
+    <!-- <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1>Panel Heading</h1>
+        </div>
+      <div class="panel-body">Panel Content</div>
+    </div> -->
+
+    <!-- layer tree -->
+    <!-- <div class="panel panel-primary">
+      <div class="panel-heading">Layers:</div>
+      <div class="panel-body">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitches">
+          <label class="custom-control-label" for="customSwitches">OpenStreetMap</label>
+        </div>
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitches1" checked="">
+          <label class="custom-control-label" for="customSwitches1">Google Satellite</label>
+        </div>
+      </div>
+    </div> -->
+
     <!-- where the map is ploted -->
     <div id="map" class="map"></div>
+
   </div>
 </template>
 
@@ -21,21 +45,61 @@ export default {
   },
   methods: {
     initMap: function () {
+      var layerOSM = new TileLayer({
+        source: new XYZ({
+          url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        })
+      })
+
+      var layerGoogleSatellite = new TileLayer({
+        source: new XYZ({
+          url: 'http://mt1.google.com/vt/lyrs=s&hl=pl&&x={x}&y={y}&z={z}'
+          // visible: false
+        })
+      })
+
       // eslint-disable-next-line
       var map = new Map({
         target: 'map',
         layers: [
-          new TileLayer({
-            source: new XYZ({
-              url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-          })
+          layerOSM, layerGoogleSatellite
         ],
         view: new View({
+          projection: 'EPSG:4326',
           center: [0, 0],
           zoom: 5
         })
       })
+
+      // var googleLayerRoadNames=new ol.layer.Tile({
+      //     title: "Google Road Names",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}' }),
+      // })
+
+      // var googleLayerRoadmap=new ol.layer.Tile({
+      //     title: "Google Road Map",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}' }),
+      // })
+
+      // var googleLayerHybrid =new ol.layer.Tile({
+      //     title: "Google Satellite & Roads",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}' }),
+      // })
+
+      // var googleLayerTerrain =new ol.layer.Tile({
+      //     title: "Google Terrain",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=t&x={x}&y={y}&z={z}' }),
+      // })
+
+      // var googleLayerHybrid2 =new ol.layer.Tile({
+      //     title: "Google Terrain & Roads",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}' }),
+      // })
+
+      // var googleLayerOnlyRoad=new ol.layer.Tile({
+      //     title: "Google Road without Building",
+      //     source: new ol.source.TileImage({ url: 'http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}' }),
+      // })
     }
   },
   mounted: function () {
---
+++ src/views/ExploreLayerTree.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="explore_layer_tree">
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <!-- <h1>Panel Heading</h1> -->
+        </div>
+      <div class="panel-body">Panel Content</div>
+    </div>
+
+    <!-- layer tree -->
+    <div class="panel panel-primary">
+      <div class="panel-heading">Layers:</div>
+      <div class="panel-body">
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitches">
+          <label class="custom-control-label" for="customSwitches">OpenStreetMap</label>
+        </div>
+        <div class="custom-control custom-switch">
+          <input type="checkbox" class="custom-control-input" id="customSwitches1" checked="">
+          <label class="custom-control-label" for="customSwitches1">Google Satellite</label>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>

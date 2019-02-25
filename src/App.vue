9658249b29ccc45d5cec8b4830bd5629da4c56e6
Rodrigo Mariano <rodmariano13@gmail.com>
2019-02-25 11:52:15 -0300

    Added a new navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
+
+    <!-- old navbar -->
+    <!-- <div id="nav">
       <router-link to="/home">Home</router-link> |
       <router-link to="/explore">Explore</router-link> |
       <router-link to="/login">Login</router-link> |
@@ -8,8 +10,44 @@
       <router-link to="/vue/home">VueHome</router-link> |
       <router-link to="/vue/about">VueAbout</router-link>
       ]
+    </div> -->
+
+    <div id="nav">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">MS3</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <!-- / routes -->
+            <li class="nav-item">
+              <router-link class="nav-link" to="/home">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/explore">Explore</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <!-- /vue routes -->
+            <li class="nav-item">
+              <router-link class="nav-link" to="/vue/home">[ VueHome</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/vue/about">VueAbout ]</router-link>
+            </li>
+            <!-- <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li> -->
+          </ul>
+        </div>
+      </nav>
     </div>
-    <router-view/>  <!-- insert the related view here -->
+
+    <!-- insert the related view here -->
+    <router-view/>
+
   </div>
 </template>
 
@@ -22,7 +60,7 @@
   color: #2c3e50;
 }
 #nav {
-  padding: 30px;
+  padding-bottom: 30px;
 }
 
 #nav a {
@@ -31,6 +69,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #00eeff;
 }
 </style>

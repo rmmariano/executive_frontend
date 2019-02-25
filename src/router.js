3665338de0153cb5742813544f3d1482e69f467c
Rodrigo Mariano <rodmariano13@gmail.com>
2019-02-25 11:00:54 -0300

    Updating App.vue

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -8,18 +9,37 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // index
     {
       path: '/',
-      name: 'home',
+      name: 'index',
+      component: Home
+    },
+
+    // /vue routes
+    {
+      path: '/vue/home',
+      name: 'vue_home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/vue/about',
+      name: 'vue_about',
+      component: About
     }
+
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: Home
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })

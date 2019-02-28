731cdbdbf23066f908a91d1fa9a518f5a46eb03d
Rodrigo Mariano <rodmariano13@gmail.com>
2019-02-28 11:35:04 -0300

    Created a vertical accordion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-// import 'bootstrap'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
@@ -8,10 +9,26 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'jquery'
+// import Popper from 'popper.js'
+
+// window.jQuery = window.$ = require('jquery')
+// window.Popper = Popper
+
+// (<any>window).Popper = Popper
+
+// window.$ = window.jQuery = require('jquery')
+// window.Popper = require('popper.js').default // pay attention to "default"
+
+// window.$ = window.jQuery = require('jquery')
 
 Vue.use(BootstrapVue)
+
+// Vue.use({
+//   install: function (Vue, options) {
+//     Vue.prototype.$jQuery = require('jquery') // you'll have this.$jQuery anywhere in your vue project
+//   }
+// })
 
 Vue.config.productionTip = false
 
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+// const webpack = require('webpack')
+
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       new webpack.ProvidePlugin({
+//         $: 'jquery',
+//         jquery: 'jquery',
+//         'window.jQuery': 'jquery',
+//         jQuery: 'jquery',
+//         Popper: ['popper.js', 'default'],
+//         // In case you imported plugins individually, you must also require them here:
+//         Util: 'exports-loader?Util!bootstrap/js/dist/util',
+//         Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown'
+//       })
+//     ]
+//   }
+// }
